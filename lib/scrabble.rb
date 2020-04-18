68b52fdaf74157476657ab9503c7431b5980acee
KOlofinmoyin <kehindeolofinmoyin@hotmail.com>
2020-04-18 22:20:47 +0100

    refactored ternary operator for a 'hash' dictionary

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,10 +1,23 @@
 class Scrabble
   def initialize(word)
     @word = word
+    @score = 0
   end
 
   def score
     @word.downcase!
-    @word == 'a' ? 1 : @word == 'd' ? 2 : @word == 'm' ? 3 : @word == 'w' ? 4 : @word == 'k' ? 5 : @word == 'x' ? 8 : 10
+    @word.each_char { |letter|
+      letters = {
+        'a':1,
+        'd':2,
+        'm':3,
+        'w':4,
+        'k':5,
+        'x':8,
+        'q':10
+      }
+      @score = letters[letter.to_sym]
+    }
+    @score
   end
 end
